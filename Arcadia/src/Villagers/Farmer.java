f380cc3ed207dfd30bc201b184aa2eb4728d4e32
Mrinal Verma <mrinalverma1993@gmail.com>
2025-07-16 17:00:24 +0200

    Created Farmer Class and Profession Enum

--- Arcadia/src/Villagers/Farmer.java
+++ Arcadia/src/Villagers/Farmer.java
@@ -1,4 +1,22 @@
 package Villagers;
 
-public class Farmer {
+public class Farmer extends Villager {
+
+    public Farmer(String firstName, String lastName, int age) {
+        super(firstName, lastName, age);
+        // assigning profession as Farmer using the Enum.
+        this.profession = Profession.FARMER;
+    }
+
+    // Generates +5 resource every "tick" if assigned to a farm, otherwise only generates +1 resource for any other building.
+    // The building determines the resource being generated.
+    @Override
+    protected void performRole() {
+        if (getAssignedBuilding() != null ) { // TODO: replace with getAssignedBuilding instanceof() Farm
+            // TODO: ResourceManager.addResource(5)
+        } else {
+            // TODO: ResourceManager.addResource(1)
+        }
+    }
 }
+`
