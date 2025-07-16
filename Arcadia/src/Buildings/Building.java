afcad83f2f452f549102c67a731d6dc6f4d6a018
Mrinal Verma <mrinalverma1993@gmail.com>
2025-07-16 19:27:33 +0200

    Added abstract building class

---
+++ Arcadia/src/Buildings/Building.java
@@ -0,0 +1,93 @@
+package Buildings;
+
+// base abstract parent class that all subclasses of buildings will inherit from.
+
+import Villagers.Villager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Building {
+    protected String name;
+    protected String territory; //TODO: Change to Territory territory once Territory class is defined
+    protected int maxCapacity;
+    protected int currentCapacity;
+    protected List<Villager> assignedVillagers;
+    protected int materialCost;
+    protected int energyUpkeepPerTick;
+
+    // default constructor - materialCost and energyUpkeepPerTick to be defined by subclasses
+    // TODO: Change String Territory to Territory territory once Territory class is defined
+    public Building(String name, String territory) {
+        this.name = name;
+        this.territory = territory;
+        maxCapacity = 5;
+        currentCapacity = 0;
+        assignedVillagers = new ArrayList<>();
+    }
+
+    // getters
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTerritory() {
+        return territory;
+    }
+
+    public int getMaxCapacity() {
+        return maxCapacity;
+    }
+
+    public int getCurrentCapacity() {
+        return currentCapacity;
+    }
+
+    public List<Villager> getAssignedVillagers() {
+        return assignedVillagers;
+    }
+
+    public int getMaterialCost() {
+        return materialCost;
+    }
+
+    public int getEnergyUpkeepPerTick() {
+        return energyUpkeepPerTick;
+    }
+
+    // setters
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCurrentCapacity(int currentCapacity) {
+        this.currentCapacity = currentCapacity;
+    }
+
+    // helper methods
+    public void assignVillager(Villager villager) {
+        assignedVillagers.add(villager);
+    }
+
+    public void removeVillager(Villager villager) {
+        assignedVillagers.remove(villager);
+    }
+
+    public boolean isFull() {
+        return currentCapacity >= maxCapacity;
+    }
+
+    public boolean isEmpty() {
+        return currentCapacity <= 0;
+    }
+
+    public int getCurrentStaffCount() {
+        return assignedVillagers.size();
+    }
+
+    // abstract method for subclasses to be able to produce resources
+    public abstract int operate();
+
+}
