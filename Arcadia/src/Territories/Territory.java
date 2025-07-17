6b55b6bc4363a8eb9a76ed6946c0efed52a47004
Mrinal Verma <mrinalverma1993@gmail.com>
2025-07-17 17:26:39 +0200

    Created Abstract Building Class

---
+++ Arcadia/src/Territories/Territory.java
@@ -0,0 +1,96 @@
+package Territories;
+
+import Buildings.Building;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Territory {
+    protected String name;
+    protected int maxBuildings;
+    protected List<Building> buildings;
+    protected int expansionCost; // Cost for a player to expand territory which increases maxBuildings.
+
+    public Territory(String name) {
+        this.name = name;
+        maxBuildings = 5;
+        expansionCost = 200;
+        this.buildings = new ArrayList<>();
+    }
+
+    // getters
+
+    public int getCurrentBuildings() {
+        return buildings.size();
+    }
+
+    public int getRemainingBuildings() {
+        return maxBuildings - buildings.size();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Building> getBuildings() {
+        return buildings;
+    }
+
+    public int getExpansionCost() {
+        return expansionCost;
+    }
+
+    // setters
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // helper methods
+
+    // allows territories to expand costing expansionCost and have increased capacity
+    public void expand(int additionalCapacity) {
+        maxBuildings += additionalCapacity;
+        // increases cost of expansion each time territories expand.
+        expansionCost += 300;
+    }
+
+    public void addBuilding(Building building) {
+        if (buildings.size() < maxBuildings) {
+            buildings.add(building);
+        } else {
+            System.out.println("Territory is full.");
+        }
+    }
+
+    public void removeBuilding(Building building) {
+        buildings.remove(building);
+    }
+
+    public boolean isFull() {
+        return buildings.size() >= maxBuildings;
+    }
+
+    // abstract method that returns a list of classes that are a subclass of the Building abstract class.
+    // the classes of buildings that are allowed are defined in the subclasses of territories.
+    public abstract List<Class<? extends Building>> getAllowedBuildingTypes();
+
+    public boolean canAddBuilding(Building building) {
+        if (getAllowedBuildingTypes().contains(building.getClass())) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+}
+
+
+
+
+
+
+
+
+
+
+}
